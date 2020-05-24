eeddc6c45910cc86f6833e66380291b6e17f5d22
1JeanCarlo2 <doritado@hotmail.com>
2020-05-24 20:25:06 -0300

    deletando pastas

--- client/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-// Reneriza na div 'root' na index.html os componentes chamados pelo App.js.
-ReactDOM.render(<App />, document.getElementById('root'));
